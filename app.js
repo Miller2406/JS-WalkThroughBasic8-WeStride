7d9a719ccbcd8a1a22d037ea6ec2b56b5add7fb2
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-09 06:33:33 +0700

    update

--- app.js
+++ app.js
@@ -11,30 +11,31 @@ const alphabet = alphabetB.map(element => {
 function hackPass(password) {
     let targetResult = [];
     for (const s of password) {
-        if (s !== ' ') {
-            let sIndex = alphabet.indexOf(s);
-            let targetIndex = sIndex + 5;
 
-            if (targetIndex <= alphabet.length - 1) {
-                targetValue = alphabet[targetIndex];
-                targetResult.push(targetValue);
+        if (s !== ' ') { //ส่วนนี้ตรวจสอบช่องว่าง ถ้ามีให้ข้ามไป ถ้าเป็นอักษร ให้ทำในนี้
+            let sIndex = alphabet.indexOf(s); // หาลำดับมันก่อน
+            let targetIndex = sIndex + 5; // เคลื่อนไปอีก 5 นั่นหละค่าที่เราต้องการแก้รหัส
+
+            if (targetIndex <= alphabet.length - 1) { // ตรวจสอบว่า ตำแหน่งที่จะไป มันเกิดกว่า 0-25 26 ตัวไหม
+                targetValue = alphabet[targetIndex]; // 0-25 ดูว่าจะตรงกับตัวอะไร
+                targetResult.push(targetValue); // อัดมันเข้าไปในผลลัพธ์
             } else {
-                let subIndex = (targetIndex - (alphabet.length))
-                let subValue = alphabet[subIndex];
-                targetResult.push(subValue);
+                let subIndex = (targetIndex - (alphabet.length)) // พวกเกินกว่า 0-25 ลบมันออกด้วย 26 ไปเริ่มนับ 0 ใหม่
+                let subValue = alphabet[subIndex]; // หาดูดิ๊ว่าตรงกับตัวไหน พวกที่เกิน z มา
+                targetResult.push(subValue); // อัดมันเข้าไปในผลลัพธ์
             }
-        } else {
-            targetResult.push(' ')
+        } else { // ส่วนนี้ข้ามมาจากตรวจช่องว่างพบ
+            targetResult.push(' ') // push ช่องว่างเข้าไป
         }
 
     }
-    return targetResult.join('');
+    return targetResult.join(''); // join ซะหน่อย แปลง array เป็นข้อความต่อเนื่องที่ตัวติดๆกัน
 
 }
 
-const tR = hackPass('d gjqz tjp ojj hpxc')
+const tR = hackPass('d gjqz tjp ojj hpxc') // คัดลอก เข้าไปเป็นตัวแปร tR จะได้เรียกดูบน console ได้
 console.log(tR);
 
-console.log(hackPass('d gjqz tjp ojj hpxc'));
+console.log(hackPass('d gjqz tjp ojj hpxc')); // พวกนี้มันพวกผี แสดงให้เห็นแต่จับต้องไม่ได้
 console.log(hackPass('xvo'));
-console.log(hackPass('ht ivhz dn w'));
+console.log(hackPass('ht ivhz dn w')); 
